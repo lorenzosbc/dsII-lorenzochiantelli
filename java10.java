e0a97e5d58dfe0ace84d8852a864f0c7524d8bea
lorenzosbc <salvatorebizuli@gmail.com>
2025-03-25 14:33:39 -0300

    firt commit

---
+++ java10.java
@@ -0,0 +1,17 @@
+
+public class Ex10 {
+    public static void main(String[] args) {
+        int primeiro = 0;
+        int segunto = 1;
+        int proximo;
+
+        for(int a=0;a<10;a++){
+            proximo = primeiro + segunto;
+
+            primeiro = segunto;
+            segunto = proximo;
+            System.out.println(primeiro);
+
+        }
+    }
+}
---
+++ java3.java
@@ -0,0 +1,8 @@
+
+public class Ex3 {
+    public static void main(String[] args) {
+        for(int a=0;a <=10;a++){
+            System.out.println(a);
+        }
+    }
+}
